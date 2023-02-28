f315362b20b83aa0a3328f433a066848d5401aa4
tmcamillo <imeio.tmc@gmail.com>
2023-02-28 20:18:00 +0100

    project setup and layout done

---
+++ README.md
@@ -0,0 +1,9 @@
+## Project created with the followings technologies:
+
+- Vanilla JS
+- HTML5
+- SCSS
+
+To recompile any SCSS changes run the command:
+
+### `npm run watch`
---
+++ src/app.js
@@ -0,0 +1,64 @@
+const header = document.querySelector('header');
+const dropdownsTriggers = document.querySelectorAll('.dropdown > button');
+const dropdownsList = document.querySelectorAll('.dropdown');
+
+window.addEventListener('scroll', function () {
+  paintedHeader();
+  createParallax();
+});
+
+dropdownsList.forEach((container) => {
+  container.addEventListener('click', function (element) {
+    toggleDropdownState(element);
+  });
+});
+
+function paintedHeader() {
+  if (document.documentElement.scrollTop == 0) {
+    header.classList.remove('bg-painted');
+    toggleTriggersClass('remove');
+  } else {
+    header.classList.add('bg-painted');
+    toggleTriggersClass('add');
+  }
+}
+
+function toggleTriggersClass(action) {
+  dropdownsTriggers.forEach((trigger) => {
+    action === 'remove'
+      ? trigger.classList.remove('bg-painted')
+      : trigger.classList.add('bg-painted');
+  });
+}
+
+function toggleDropdownState(element) {
+  const trigger = element.target;
+  const menu = trigger.parentNode.querySelector('.menu__itemDropdown');
+
+  let isOpen = trigger.dataset.expanded;
+  let iconDropDown = trigger.querySelector('.fa-solid');
+
+  if (isOpen === 'true') {
+    iconDropDown.classList.remove('fa-chevron-up');
+    iconDropDown.classList.add('fa-chevron-down');
+    trigger.setAttribute('data-expanded', 'false');
+    menu.style.visibility = 'hidden';
+    menu.style.opacity = '0';
+  } else {
+    iconDropDown.classList.remove('fa-chevron-down');
+    iconDropDown.classList.add('fa-chevron-up');
+    trigger.setAttribute('data-expanded', 'true');
+    menu.style.visibility = 'visible';
+    menu.style.opacity = '1';
+  }
+}
+
+function createParallax() {
+  const target = document.querySelectorAll(
+    '.highlight__text, .highlight__button'
+  );
+  target.forEach((_item, index) => {
+    const pos = window.pageYOffset * target[index].dataset.rate;
+    target[index].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+  });
+}
